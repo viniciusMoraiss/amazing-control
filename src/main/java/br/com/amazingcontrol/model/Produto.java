9d27063056f7e81d791937ff51bc88fecba448d8
Felipe Renan <feelipe.renan@gmail.com>
2015-09-12 13:34:51 -0300

    Commit Inicial do projeto

---
+++ src/main/java/br/com/amazingcontrol/model/Produto.java
@@ -0,0 +1,5 @@
+package br.com.amazingcontrol.model;
+
+public class Produto {
+
+}
---
+++ src/test/java/br/com/amazingcontrol/model/ProdutoTest.java
@@ -0,0 +1,14 @@
+package br.com.amazingcontrol.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ProdutoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
