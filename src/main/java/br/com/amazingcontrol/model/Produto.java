7b8e09c98d7845e1a19f514cdf44892648f63650
Felipe Renan <feelipe.renan@gmail.com>
2015-09-12 19:40:19 -0300

    Criada varivel nome na classe Produto, os metodos de acesso e modificador.

--- src/main/java/br/com/amazingcontrol/model/Produto.java
+++ src/main/java/br/com/amazingcontrol/model/Produto.java
@@ -5,4 +5,21 @@ Criado a classe produto
 */
 public class Produto {
 
+	private String nome;
+	
+	/**
+	 * Recupera nome
+	 * @return nome
+	 */
+	public String getNome() {
+		return this.nome;
+	}
+	
+	/**
+	 * Configura nome
+	 * @param nome
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
