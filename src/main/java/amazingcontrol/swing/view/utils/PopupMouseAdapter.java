bc1b515072a6e3f5f8a5f1f82597200084a0e789
Felipe Renan <feelipe.renan@gmail.com>
2015-11-06 22:02:28 -0200

    Classe helper para eventos do mouse

---
+++ src/main/java/amazingcontrol/swing/view/utils/PopupMouseAdapter.java
@@ -0,0 +1,28 @@
+package amazingcontrol.swing.view.utils;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JPopupMenu;
+
+public class PopupMouseAdapter extends MouseAdapter {
+
+    private JPopupMenu menu;
+
+    public PopupMouseAdapter(JPopupMenu menu) {
+        super();
+        this.menu = menu;
+    }
+
+    public void mousePressed(MouseEvent evt) {
+        if (evt.isPopupTrigger()) {
+            menu.show(evt.getComponent(), evt.getX(), evt.getY());
+        }
+    }
+
+    public void mouseReleased(MouseEvent evt) {
+        if (evt.isPopupTrigger()) {
+            menu.show(evt.getComponent(), evt.getX(), evt.getY());
+        }
+    }
+}
