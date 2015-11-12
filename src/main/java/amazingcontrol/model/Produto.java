eef21ab4d8a4ad0b8c71d4e3179776de9ac144ae
maycon <mayconmustaine@hotmail.com>
2015-11-12 21:40:35 -0200

    coloquei todos os getters e setters na classe, mas falta o metodo ToString

--- src/main/java/amazingcontrol/model/Produto.java
+++ src/main/java/amazingcontrol/model/Produto.java
@@ -1,23 +1,69 @@
 package amazingcontrol.model;
 
-
 public class Produto {
 
 	private String nome;
-	
+	private String marca;
+	private String tipo;
+	private double valorCusto;
+	private double valorVenda;
+	private int quantidadeDeProduto;
+
 	/**
 	 * Recupera nome
+	 * 
 	 * @return nome
 	 */
 	public String getNome() {
 		return this.nome;
 	}
-	
+
 	/**
 	 * Configura nome
+	 * 
 	 * @param nome
 	 */
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return this.marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getTipo() {
+		return this.tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getValorCusto() {
+		return this.valorCusto;
+	}
+
+	public void setValorCusto(double valorCusto) {
+		this.valorCusto = valorCusto;
+	}
+
+	public double getValorVenda() {
+		return this.valorVenda;
+	}
+
+	public void setValorVenda(double valorVenda) {
+		this.valorVenda = valorVenda;
+	}
+
+	public int getQuantidadeDeProduto() {
+		return this.quantidadeDeProduto;
+	}
+
+	public void setQuantidadeDeProduto(int quantidadeDeProduto) {
+		this.quantidadeDeProduto = quantidadeDeProduto;
 	}
 }
