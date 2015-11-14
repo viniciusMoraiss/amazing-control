32bddd70b4fa26373d3db53754bdd01cbeaf995a
Bruno Martins <brubru413@gmail.com>
2015-11-14 14:20:09 -0200

    correção de fornecedor para "fornecedores " e cep e uf de int para String seguindo o modelo do banco

--- src/main/java/amazingcontrol/dao/FornecedorDAO.java
+++ src/main/java/amazingcontrol/dao/FornecedorDAO.java
@@ -13,16 +13,16 @@ public class FornecedorDAO implements Crud<Fornecedor> {
 
 	public void inserir(Fornecedor fornecedor) {
 		Connection con = ConexaoMySQL.conectar();
-		String sql = "INSERT INTO fornecedor (nome, telefone, endereço, cidade, cep, uf) VALUES (?,?,?,?,?,?)";
+		String sql = "INSERT INTO fornecedores (nome, telefone, endereco, cidade, cep, uf) VALUES (?,?,?,?,?,?)";
 		PreparedStatement stmt = null;
-		
+
 		try {
 			stmt = con.prepareStatement(sql);
 			stmt.setString(1, fornecedor.getNome());
-			stmt.setInt(2, fornecedor.getTelefone());
+			stmt.setString(2, fornecedor.getTelefone());
 			stmt.setString(3, fornecedor.getEndereco());
 			stmt.setString(4, fornecedor.getCidade());
-			stmt.setInt(5, fornecedor.getCep());
+			stmt.setString(5, fornecedor.getCep());
 			stmt.setString(6, fornecedor.getUf());
 			stmt.execute();
 		} catch (Exception e) {
@@ -34,7 +34,7 @@ public class FornecedorDAO implements Crud<Fornecedor> {
 
 	public void deletar(Fornecedor fornecedor) {
 		Connection con = ConexaoMySQL.conectar();
-		String sql = "DELETE FROM fornecedor WHERE id = ? ";
+		String sql = "DELETE FROM fornecedores WHERE id = ? ";
 		PreparedStatement stmt = null;
 		try {
 			stmt = con.prepareStatement(sql);
@@ -49,15 +49,15 @@ public class FornecedorDAO implements Crud<Fornecedor> {
 
 	public void atualizar(Fornecedor fornecedor) {
 		Connection con = ConexaoMySQL.conectar();
-		String sql = "UPDATE INTO fornecedor (nome, telefone, endereço, cidade, cep, uf) VALUES (?,?,?,?,?,?)";
+		String sql = "UPDATE INTO fornecedores (nome, telefone, endereço, cidade, cep, uf) VALUES (?,?,?,?,?,?)";
 		PreparedStatement stmt = null;
 		try {
 			stmt = con.prepareStatement(sql);
 			stmt.setString(1, fornecedor.getNome());
-			stmt.setInt(2, fornecedor.getTelefone());
+			stmt.setString(2, fornecedor.getTelefone());
 			stmt.setString(3, fornecedor.getEndereco());
 			stmt.setString(4, fornecedor.getCidade());
-			stmt.setInt(5, fornecedor.getCep());
+			stmt.setString(5, fornecedor.getCep());
 			stmt.setString(6, fornecedor.getUf());
 			stmt.execute();
 		} catch (Exception e) {
@@ -73,7 +73,7 @@ public class FornecedorDAO implements Crud<Fornecedor> {
 		String sql = " SELECT * FROM Fornecedores";
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			stmt = con.prepareStatement(sql);
 			rs = stmt.executeQuery();
@@ -83,23 +83,23 @@ public class FornecedorDAO implements Crud<Fornecedor> {
 				fornecedor = new Fornecedor();
 				fornecedor.setId(rs.getInt("id"));
 				fornecedor.setNome(rs.getString("nome"));
-				fornecedor.setTelefone(rs.getInt("telefone"));
+				fornecedor.setTelefone(rs.getString("telefone"));
 				fornecedor.setEndereco(rs.getString("endereço"));
 				fornecedor.setCidade(rs.getString("cidade"));
-				fornecedor.setCep(rs.getInt("cep"));
+				fornecedor.setCep(rs.getString("cep"));
 				fornecedor.setUf(rs.getString("uf"));
 
 				fornecedores.add(fornecedor);
 			}
-			
+
 			return fornecedores;
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			ConexaoMySQL.desconectar(con, stmt, rs);
 		}
-		
+
 		return fornecedores;
 	}
 }
