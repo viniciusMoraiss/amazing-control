8d78539ac88b331e7d009c9997b5a085a2f1e194
Bruno Martins <brubru413@gmail.com>
2015-11-14 14:16:57 -0200

    faz a conexao entre a tela e o banco de dados

---
+++ src/main/java/amazingcontrol/service/FornecedorService.java
@@ -0,0 +1,50 @@
+package amazingcontrol.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import amazingcontrol.dao.FornecedorDAO;
+import amazingcontrol.model.Fornecedor;
+
+public class FornecedorService {
+	private FornecedorDAO dao;
+
+	/*
+	 * Cria acesso ao metodos do banco já no construtor
+	 */
+	public FornecedorService() throws SQLException {
+		dao = new FornecedorDAO();
+	}
+
+	/*
+	 * metodo para carregar lista de fornecedor
+	 */
+	public List<Fornecedor> listar() {
+		return dao.lista();
+	}
+
+	/*
+	 * Metodo para salvar fornecedor no banco de dados, respeitando a logica: -
+	 * ira validar os campos - caso o id seja igual a null ira inserir o
+	 * fornecedor - caso o id seja diferente de null ira atualizar um fornecedor
+	 * existente
+	 */
+	public void salvar(Fornecedor fornecedor) throws Exception {
+		// valida fornecedor antes de inserir ou atualizar
+		// validar(fornecedor);
+
+		if (fornecedor.isNullId()) {
+			dao.inserir(fornecedor);
+		} else {
+			dao.atualizar(fornecedor);
+		}
+	}
+
+	/*
+	 * Metodo para deletar fornecedor
+	 */
+	public void deletar(Fornecedor fornecedor) {
+		dao.deletar(fornecedor);
+	}
+
+}
