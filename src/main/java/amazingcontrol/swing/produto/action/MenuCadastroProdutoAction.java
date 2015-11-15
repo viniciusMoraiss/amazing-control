b561156c369af7e9c3f523e3999e1145d3bf280e
maycon <mayconmustaine@hotmail.com>
2015-11-15 10:32:35 -0200

    menuCadastroProduto e TelaCadastroProdutos criadas

--- src/main/java/amazingcontrol/swing/produto/action/MenuCadastroProdutoAction.java
+++ src/main/java/amazingcontrol/swing/produto/action/MenuCadastroProdutoAction.java
@@ -1,24 +1,57 @@
 package amazingcontrol.swing.produto.action;
 
+import static javax.swing.JOptionPane.ERROR_MESSAGE;
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+import static javax.swing.JOptionPane.showMessageDialog;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
 
 import javax.swing.JFrame;
 
+import amazingcontrol.model.Fornecedor;
+import amazingcontrol.model.Produto;
+import amazingcontrol.model.UF;
+import amazingcontrol.service.FornecedorService;
+import amazingcontrol.service.ProdutoService;
+import amazingcontrol.swing.fornecedor.view.TelaCadastroFornecedor;
 import amazingcontrol.swing.principal.view.TelaPrincipal;
 import amazingcontrol.swing.produto.view.TelaCadastroProduto;
 
 public class MenuCadastroProdutoAction implements ActionListener {
 	
-	private JFrame mainView;
+	private TelaCadastroProduto view;
 
-	public MenuCadastroProdutoAction(TelaPrincipal mainView) {
-       this.mainView = mainView;
+	public void CriarProdutoAction(TelaCadastroProduto view) {
+		this.view = view;
 	}
 
-	@Override
 	public void actionPerformed(ActionEvent e) {
-		new TelaCadastroProduto(mainView).setVisible(true);
+		// pega os atributos digitados pelo usuario e adiciona a uma variavel
+		String nome = view.getNomeText().getText();
+		String marca = view.getMarcaText().getText();
+		String tipo = view.getTipoText().getText();
+		double valorCusto = Double.parseDouble(view.getValorCustoText().getText());
+		double valorVenda = Double.parseDouble(view.getValorVendaText().getText());
+		int quantidadeDeProduto = Integer.parseInt(view.getValorVendaText().getText());
+		
+	
+		
+		try {
+			// cria objeto com os dados digitados pelo usuario
+			Produto produto = new Produto();
+			
+			// tenta salvar o objeto fornecedor no banco de dados
+			new ProdutoService().salvar(produto);
+			
+			// mensagem de sucesso
+			showMessageDialog(view, "Inserido com sucesso", "OK", INFORMATION_MESSAGE);
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			showMessageDialog(view, ex.getMessage(), "Erro", ERROR_MESSAGE);
+		}
 	}
 
 }
--- src/main/java/amazingcontrol/swing/produto/view/TelaCadastroProduto.java
+++ src/main/java/amazingcontrol/swing/produto/view/TelaCadastroProduto.java
@@ -1,25 +1,263 @@
 package amazingcontrol.swing.produto.view;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.text.ParseException;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
+
+import amazingcontrol.model.UF;
+import amazingcontrol.swing.view.utils.CustomizeView;
 
 public class TelaCadastroProduto extends JDialog {
-	
+
+	private JLabel nomeLabel;
+	private JTextField nomeText;
+	private JLabel marcaLabel;
+	private JTextField marcaText;
+	private JLabel tipoLabel;
+	private JTextField tipoText;
+	private JLabel valorCustoLabel;
+	private JTextField valorCustoText;
+	private JLabel valorVendaLabel;
+	private JTextField valorVendaText;
+	private JLabel quantidadeDeProdutoLabel;
+	private JTextField quantidadeDeProdutoText;
+	private JButton cadastrarBt;
+	private JButton cancelarBt;
+
 	public TelaCadastroProduto() {
 		this(null);
 	}
 
-	public TelaCadastroProduto(JFrame mainView) {
+	public TelaCadastroProduto(JFrame view) {
 		super();
-		
-		setTitle("[A-CONTROL] Cadastro de usuarios");
-		setSize(630, 320);
+
+		initComponents();
+		initPainel();
+		// initListeners();
+
+		setTitle(("[A-CONTROL] Fornecedor"));
+		setSize(630, 470);
 		setResizable(false);
-		setLocationRelativeTo(mainView);
-		//setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		setModalityType(ModalityType.DOCUMENT_MODAL);
-		setAlwaysOnTop(true);
-		setModal(true);
+		setVisible(true);
+		setLocationRelativeTo(null);
 	}
 
+	// getters and setters
+
+	public JLabel getNomeLabel() {
+		return nomeLabel;
+	}
+
+	public void setNomeLabel(JLabel nomeLabel) {
+		this.nomeLabel = nomeLabel;
+	}
+
+	public JTextField getNomeText() {
+		return nomeText;
+	}
+
+	public void setNomeText(JTextField nomeText) {
+		this.nomeText = nomeText;
+	}
+
+	public JLabel getMarcaLabel() {
+		return marcaLabel;
+	}
+
+	public void setMarcaLabel(JLabel marcaLabel) {
+		this.marcaLabel = marcaLabel;
+	}
+
+	public JTextField getMarcaText() {
+		return marcaText;
+	}
+
+	public void setMarcaText(JTextField marcaText) {
+		this.marcaText = marcaText;
+	}
+
+	public JLabel getTipoLabel() {
+		return tipoLabel;
+	}
+
+	public void setTipoLabel(JLabel tipoLabel) {
+		this.tipoLabel = tipoLabel;
+	}
+
+	public JTextField getTipoText() {
+		return tipoText;
+	}
+
+	public void setTipoText(JTextField tipoText) {
+		this.tipoText = tipoText;
+	}
+
+	public JLabel getValorCustoLabel() {
+		return valorCustoLabel;
+	}
+
+	public void setValorCustoLabel(JLabel valorCustoLabel) {
+		this.valorCustoLabel = valorCustoLabel;
+	}
+
+	public JTextField getValorCustoText() {
+		return valorCustoText;
+	}
+
+	public void setValorCustoText(JTextField valorCustoText) {
+		this.valorCustoText = valorCustoText;
+	}
+
+	public JLabel getValorVendaLabel() {
+		return valorVendaLabel;
+	}
+
+	public void setValorVendaLabel(JLabel valorVendaLabel) {
+		this.valorVendaLabel = valorVendaLabel;
+	}
+
+	public JTextField getValorVendaText() {
+		return valorVendaText;
+	}
+
+	public void setValorVendaText(JTextField valorVendaText) {
+		this.valorVendaText = valorVendaText;
+	}
+
+	public JLabel getQuantidadeDeProdutoLabel() {
+		return quantidadeDeProdutoLabel;
+	}
+
+	public void setQuantidadeDeProdutoLabel(JLabel quantidadeDeProdutoLabel) {
+		this.quantidadeDeProdutoLabel = quantidadeDeProdutoLabel;
+	}
+
+	public JTextField getQuantidadeDeProdutoText() {
+		return quantidadeDeProdutoText;
+	}
+
+	public void setQuantidadeDeProdutoText(JTextField quantidadeDeProdutoText) {
+		this.quantidadeDeProdutoText = quantidadeDeProdutoText;
+	}
+
+	public JButton getCadastrarBt() {
+		return cadastrarBt;
+	}
+
+	public void setCadastrarBt(JButton cadastrarBt) {
+		this.cadastrarBt = cadastrarBt;
+	}
+
+	public JButton getCancelarBt() {
+		return cancelarBt;
+	}
+
+	public void setCancelarBt(JButton cancelarBt) {
+		this.cancelarBt = cancelarBt;
+	}
+
+	private void initComponents() {
+
+		nomeLabel = new JLabel("Nome ");
+		nomeText = new JTextField(20);
+		CustomizeView.labelsAndInputs(nomeLabel, nomeText);
+
+		marcaLabel = new JLabel("Marca");
+		marcaText = new JFormattedTextField(10);
+		CustomizeView.labelsAndInputs(marcaLabel, marcaText);
+
+		tipoLabel = new JLabel("Tipo");
+		tipoText = new JTextField(20);
+		CustomizeView.labelsAndInputs(tipoLabel, tipoText);
+
+		valorCustoLabel = new JLabel("Valor Custo");
+		valorCustoText = new JTextField(10);
+		CustomizeView.labelsAndInputs(valorCustoLabel, valorCustoText);
+
+		valorVendaLabel = new JLabel("Valor Venda");
+		valorVendaText = new JFormattedTextField(10);
+		CustomizeView.labelsAndInputs(valorVendaLabel, valorVendaText);
+
+		quantidadeDeProdutoLabel = new JLabel("Valor Custo");
+		quantidadeDeProdutoText = new JFormattedTextField(10);
+		CustomizeView.labelsAndInputs(quantidadeDeProdutoLabel, quantidadeDeProdutoText);
+
+		cadastrarBt = new JButton("Cadastrar");
+		cancelarBt = new JButton("Cancelar");
+
+	}
+
+	private void initPainel() {
+		JPanel painel = new JPanel(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.anchor = GridBagConstraints.WEST;
+		constraints.insets = new Insets(10, 10, 10, 10);
+
+		constraints.gridy = 0; // linha
+		painel.add(nomeLabel, constraints);
+
+		constraints.gridx = 1; // coluna
+		painel.add(nomeText, constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 2;
+		painel.add(marcaLabel, constraints);
+
+		constraints.gridx = 1;
+		painel.add(marcaText, constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 3; // linha
+		painel.add(tipoLabel, constraints);
+
+		constraints.gridx = 1;
+		painel.add(tipoText, constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 4; // linha
+		painel.add(valorCustoLabel, constraints);
+
+		constraints.gridx = 1;
+		painel.add(valorCustoText, constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 5;
+		painel.add(valorVendaLabel, constraints);
+
+		constraints.gridx = 1;
+		painel.add(valorVendaText, constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 6;
+		painel.add(quantidadeDeProdutoLabel, constraints);
+
+		constraints.gridx = 1;
+		painel.add(quantidadeDeProdutoText, constraints);
+
+		constraints.gridx = 0;
+		constraints.gridy = 7;
+		constraints.anchor = GridBagConstraints.CENTER;
+		painel.add(cadastrarBt, constraints);
+
+		constraints.gridx = 1;
+		constraints.anchor = GridBagConstraints.SOUTH;
+		painel.add(cancelarBt, constraints);
+
+		painel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Fornecedor"));
+
+		add(painel);
+
+	}
 }
