c82b73230f83ab6366c0469b3be54382782f6d96
Felipe Renan <feelipe.renan@gmail.com>
2015-11-25 17:14:05 -0200

    Modificados os menus

--- src/main/java/amazingcontrol/swing/principal/view/TelaPrincipal.java
+++ src/main/java/amazingcontrol/swing/principal/view/TelaPrincipal.java
@@ -6,9 +6,13 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 import amazingcontrol.model.Usuario;
+import amazingcontrol.swing.cliente.action.NovoClienteAction;
 import amazingcontrol.swing.cliente.action.TelaClienteAction;
+import amazingcontrol.swing.fornecedor.action.NovoFornecedorAction;
 import amazingcontrol.swing.fornecedor.action.TelaFornecedorAction;
+import amazingcontrol.swing.produto.action.NovoProdutoAction;
 import amazingcontrol.swing.produto.action.TelaProdutoAction;
+import amazingcontrol.swing.usuario.action.NovoUsuarioAction;
 import amazingcontrol.swing.usuario.action.TelaAlterarSenhaUsuarioAction;
 import amazingcontrol.swing.usuario.action.TelaUsuarioAction;
 import amazingcontrol.swing.vendas.action.TelaVendasAction;
@@ -73,22 +77,30 @@ public class TelaPrincipal extends JFrame {
 	private JMenuBar createMenu() {
 		JMenuBar menuBar;
 		menuBar = new JMenuBar();
+		
+		// todos os cadastros
+		menuCadastros(menuBar);
+		
+		// todas as consultas
+		menuConsulta(menuBar);
 
+		/*
 		// adiciona menu de produtos
 		menuProdutos(menuBar);
-
-		// adiciona menu de usuarios
-		menuOpcoesUsuario(menuBar);
 
 		// adiciona menu de fornecedores
 		menuOpcoesFornecedores(menuBar);
 		
 		// menu clientes
 		menuClientes(menuBar);
+		*/
 		
 		// menu vendas
 		menuOpcoesVendas(menuBar);
-
+		
+		// adiciona menu de usuarios
+		menuOpcoesUsuario(menuBar);
+		
 		return menuBar;
 	}
 	
@@ -108,6 +120,61 @@ public class TelaPrincipal extends JFrame {
 	}
 	
 	/**
+	 * Monta menu de produtos
+	 * @param menuBar
+	 */
+	private void menuCadastros(JMenuBar menuBar) {
+		JMenu menu;
+		JMenuItem menuItem;
+		menu = new JMenu("Cadastros");
+		menuBar.add(menu);
+		
+		
+		menuItem = new JMenuItem("Cadastrar de usuarios");
+		menuItem.addActionListener(new NovoUsuarioAction(null));
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Cadastrar de fornecedores");
+		menuItem.addActionListener(new NovoFornecedorAction(null));
+		menu.add(menuItem);
+
+		menuItem = new JMenuItem("Cadastrar de produtos");
+		menuItem.addActionListener(new NovoProdutoAction(null));
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Cadastrar de clientes");
+		menuItem.addActionListener(new NovoClienteAction(null));
+		menu.add(menuItem);
+	}
+	
+	/**
+	 * Monta menu de consultas
+	 * @param menuBar
+	 */
+	private void menuConsulta(JMenuBar menuBar) {
+		JMenu menu;
+		JMenuItem menuItem;
+		menu = new JMenu("Consultas");
+		menuBar.add(menu);
+		
+		menuItem = new JMenuItem("Consultar de Fornecedor");
+		menuItem.addActionListener(new TelaFornecedorAction(this));
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Consultar de Produtos");
+		menuItem.addActionListener(new TelaProdutoAction(this));
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Consultar de Clientes");
+		menuItem.addActionListener(new TelaClienteAction(this));
+		menu.add(menuItem);
+		
+		menuItem = new JMenuItem("Consultar de Usuarios ");
+		menuItem.addActionListener(new TelaClienteAction(this));
+		menu.add(menuItem);
+	}
+	
+	/**
 	 * Monta menu com opções do usuario
 	 * @param menuBar
 	 */
@@ -116,10 +183,12 @@ public class TelaPrincipal extends JFrame {
 		JMenuItem menuItem;
 		menu = new JMenu("Opcões de Usuario");
 		menuBar.add(menu);
-
+		
+		/*
 		menuItem = new JMenuItem("Consulta usuarios");
 		menuItem.addActionListener(new TelaUsuarioAction(this));
 		menu.add(menuItem);
+		*/
 
 		menuItem = new JMenuItem("Alterar minha senha");
 		menuItem.addActionListener(new TelaAlterarSenhaUsuarioAction(this));
